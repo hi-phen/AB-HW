3ecbd7814e1e56a6439ebce6d0435c33a83119e9
minseob.kim <minseob.kim@kurlycorp.com>
2021-12-18 15:48:29 +0900

    회원 컨트롤러에 내정보 조회 기능을 추가 합니다.

--- src/main/java/com/ms/member/web/MemberController.java
+++ src/main/java/com/ms/member/web/MemberController.java
@@ -2,22 +2,28 @@ package com.ms.member.web;
 
 import com.ms.member.exception.DuplicateValueException;
 import com.ms.member.exception.MemberNotFoundException;
+import com.ms.member.exception.UnAuthorizedException;
 import com.ms.member.mobileauth.service.ValidateMobileAuthTokenUseCase;
 import com.ms.member.mobileauth.service.command.ValidateMobileAuthTokenCommand;
 import com.ms.member.mobileauth.service.exception.InvalidMobileAuthTokenException;
 import com.ms.member.service.MemberCreateUseCase;
+import com.ms.member.service.MemberFindUseCase;
 import com.ms.member.service.command.MemberLoginCommand;
 import com.ms.member.service.command.MemberPersistCommand;
 import com.ms.member.service.impl.LoginServiceFactory;
 import com.ms.member.web.model.LoginRequest;
 import com.ms.member.web.model.MemberCreateRequest;
+import com.ms.member.web.model.MemberInfoResponse;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +37,7 @@ public class MemberController {
   private final MemberCreateUseCase memberCreateUseCase;
   private final ValidateMobileAuthTokenUseCase validateMobileAuthTokenUseCase;
   private final LoginServiceFactory loginServiceFactory;
+  private final MemberFindUseCase memberFindUseCase;
 
   /**
    * 회원 가입(생성).
@@ -82,6 +89,41 @@ public class MemberController {
       );
     } catch (MemberNotFoundException e) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+  }
+
+  /**
+   * 회원 조회 (내정보 조회).
+   *
+   * @param id    회원 아이디
+   * @param token 로그인 토큰
+   * @return 회원 정보
+   */
+  @GetMapping("/member/{id}")
+  ResponseEntity<?> getMember(@PathVariable Long id,
+                              @RequestHeader("authorization") String token) {
+    try {
+      validateLoginToken(id, token);
+      var member = memberFindUseCase.findById(id);
+      return ResponseEntity.ok(
+          MemberInfoResponse.of()
+              .email(member.getEmail())
+              .id(member.getId())
+              .mobile(member.getMobile())
+              .name(member.getName())
+              .nickName(member.getNickName())
+              .build()
+      );
+    } catch (UnAuthorizedException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    } catch (MemberNotFoundException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  private void validateLoginToken(Long id, String token) throws UnAuthorizedException {
+    if (!token.equals(String.valueOf(id))) {
+      throw new UnAuthorizedException();
     }
   }
 }
